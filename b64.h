21b551fe7795e6fe0065ed046dfc7386ff8d8feb
shinnies <ru.shinnies@yandex.com>
2022-05-04 01:11:15 +0200

    Added well-formated header file

--- b64.h
+++ b64.h
@@ -5,6 +5,16 @@
 #ifndef B64_H_
     #define B64_H_
 
+/////////////////////////////////////////////////////////
+//  b64_encode -> encode char * passed as parameter    //
+//  return value -> encoded char * (or NULL if error)  //
+/////////////////////////////////////////////////////////
 char *b64_encode(char *string_to_encode);
+
+/////////////////////////////////////////////////////////
+//  b64_decode -> decode string passed as parameter    //
+//  return value -> decoded string (or NULL if error)  //
+/////////////////////////////////////////////////////////
+char *b64_decode(char *string_to_decode);
 
 #endif
