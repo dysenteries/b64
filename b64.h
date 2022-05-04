43446ebe2650e289743d6791ea79fef23abc5cce
shinnies <ru.shinnies@yandex.com>
2022-05-04 19:29:52 +0200

    Re-format header file, added b64url functions prototypes

--- b64.h
+++ b64.h
@@ -5,16 +5,20 @@
 #ifndef B64_H_
     #define B64_H_
 
-/////////////////////////////////////////////////////////
-//  b64_encode -> encode char * passed as parameter    //
-//  return value -> encoded char * (or NULL if error)  //
-/////////////////////////////////////////////////////////
+//  b64_encode -> encode in base64 a string passed as parameter
+//  return value -> encoded string (or NULL if error)
 char *b64_encode(char *string_to_encode);
 
-/////////////////////////////////////////////////////////
-//  b64_decode -> decode string passed as parameter    //
-//  return value -> decoded string (or NULL if error)  //
-/////////////////////////////////////////////////////////
+//  b64url_encode -> encode in base64 a string passed as parameter and replace ('+' and '/') by ('-' and '_')
+//  return value -> encoded string (or NULL if error)
+char *b64url_encode(char *string_to_encode);
+
+//  b64_decode -> decode a base64 string passed as parameter
+//  return value -> decoded string (or NULL if error)
 char *b64_decode(char *string_to_decode);
+
+//  b64_decode -> decode a base64url string passed as parameter
+//  return value -> decoded string (or NULL if error)
+char *b64url_decode(char *string_to_decode);
 
 #endif
