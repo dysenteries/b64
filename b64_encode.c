dad50e8eb235353b7e4a4409180a323189d1a448
shinnies <ru.shinnies@yandex.com>
2022-05-05 22:39:42 +0200

    Removed main function

--- b64_encode.c
+++ b64_encode.c
@@ -125,10 +125,3 @@ char *b64url_encode(char *str)
     }
     return b64;
 }
-
-int main(int argc, char **argv)
-{
-    if (argc > 1)
-        printf("%s\n", b64_encode(argv[1]));
-    return 0;
-}
